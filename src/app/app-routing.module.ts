6e61197c82986b9bcdacdaa1fb26268df21d8a57
ghailen <ghailenemark@gmail.com>
2021-05-09 14:16:30 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "products", component: ProductsComponent},
+  {path: "", component: HomeComponent},
+  {path: "newProduct", component: ProductAddComponent},
+  {path: "editProduct/:id", component: ProductEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
